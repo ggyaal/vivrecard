2ed8aa323a8a86070aae0d628ad54a73cda7d0dc
ggyaal <ggyaal@gmail.com>
2025-08-26 23:02:25 +0900

    style: memberBadge grid width

--- src/pages/sub-pages/member/MemberBadges.tsx
+++ src/pages/sub-pages/member/MemberBadges.tsx
@@ -14,7 +14,7 @@ const Wrapper = styled.div``;
 
 const BadgeList = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-template-columns: repeat(auto-fit, 200px);
 `;
 
 const EmptyList = styled.div`
